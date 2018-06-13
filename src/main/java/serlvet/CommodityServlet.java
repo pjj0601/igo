52933500198f337066a05ccc1077dcdb71b55092
PaN4E <527662362@qq.com>
2018-06-14 00:25:45 +0800

    init

--- src/main/java/serlvet/CommodityServlet.java
+++ src/main/java/serlvet/CommodityServlet.java
@@ -17,7 +17,7 @@ public class CommodityServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         try {
-            response.getWriter().println("<h1>Hello Servlet!</h1>");
+            response.getWriter().println("<h1>Hello init!</h1>");
             response.getWriter().println(new Date().toLocaleString());
         } catch (IOException e) {
             // TODO Auto-generated catch block
