1c1460a594fbd1bb8ade82f0fa95fa8e82efe00b
PaN4E <527662362@qq.com>
2018-06-14 00:22:05 +0800

    init

---
+++ src/main/java/serlvet/CommodityServlet.java
@@ -0,0 +1,27 @@
+package serlvet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+
+@WebServlet(name = "CommodityServlet")
+public class CommodityServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        try {
+            response.getWriter().println("<h1>Hello Servlet!</h1>");
+            response.getWriter().println(new Date().toLocaleString());
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
