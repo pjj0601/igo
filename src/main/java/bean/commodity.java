d4b691f229bc8d9efc1dec3cb92678b9109ab3dd
PaN4E <527662362@qq.com>
2018-06-14 09:51:52 +0800

    开始文件的创建

---
+++ src/main/java/bean/commodity.java
@@ -0,0 +1,59 @@
+package bean;
+
+public class commodity {
+
+    private int id;
+    private String name;
+    private String type;
+    private String oldprice;
+    private String newprice;
+    private String info;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getOldprice() {
+        return oldprice;
+    }
+
+    public void setOldprice(String oldprice) {
+        this.oldprice = oldprice;
+    }
+
+    public String getNewprice() {
+        return newprice;
+    }
+
+    public void setNewprice(String newprice) {
+        this.newprice = newprice;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+}
---
+++ src/main/java/bean/shoppingcart.java
@@ -0,0 +1,22 @@
+package bean;
+
+public class shoppingcart {
+    private int sid;
+    private int num;
+
+    public int getSid() {
+        return sid;
+    }
+
+    public void setSid(int sid) {
+        this.sid = sid;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+}
